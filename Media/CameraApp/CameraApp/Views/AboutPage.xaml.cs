8bb30aaf15532c7265cb83aa173df0c77a62c9c3
Radek Zahradník <zahradnik@ginasystem.com>
2018-02-28 23:41:16 +0100

    Some small clean up.

---
+++ Media/CameraApp/CameraApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CameraApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
